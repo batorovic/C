a30d195d7f77dea7f26b96b4522d0b92cb4c78ea
batuhank34 <33568252+batuhank34@users.noreply.github.com>
2019-11-04 17:21:21 +0300

    çalışma
    kare çizme ve  X çizme içinde var

--- Serbest Çalışmalar/Sayıyı Tersten Yazdırma/Sayıyı Tersten Yazdırma.cpp
+++ Serbest Çalışmalar/Sayıyı Tersten Yazdırma/Sayıyı Tersten Yazdırma.cpp
@@ -4,9 +4,10 @@
 int main()
 {
 	setlocale(LC_ALL,"Turkish");
-	//sayıyı tersten yazdırma
+	/*
+	//sayÄ±yÄ± tersten yazdÄ±rma
 	int sayi,ters=0;
-	printf("Lütfen sayıyı giriniz : ");
+	printf("LÃ¼tfen sayÄ±yÄ± giriniz : ");
 	scanf("%d",&sayi);
 	
 	while(sayi != 0)
@@ -17,8 +18,95 @@ int main()
 	
 	}
 	
-	printf("Tersten: %d",ters);
+	printf("Tersten: %d",ters);*/
+
 	
+	/*
+	for(int i = 1;i<=5;i++)
+	{
+		for(int j = 1;j<=5;j++)
+		{
+			if((i+j) == 6 || i==j)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}*/
+	
+	//kare çizme
+	int k = 0;
+	printf("kenar kaç birim olsun ");
+	scanf("%d",&k);
+	
+	
+	
+	/*
+	  ******
+	  *    *
+	  *    *
+	  *    *
+	  *    *   
+	  ******
+	  
+	
+	
+	*/
+
+	
+	for(int i = 1;i<=k;i++)
+	{
+		for(int j = 1; j<=k;j++)
+		{
+			if(j == 1 || i == k || j == k || i == 1)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	
 	
